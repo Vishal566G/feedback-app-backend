22eb914ac947cb47b6e834ff417ab76cae430a0e
Vishal <visku199@gmail.com>
2026-04-16 20:48:24 +0530

    create feedback schema

---
+++ src/models/Feedback.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const feedbackSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please provide your name"],
+      trim: true,
+      maxlength: [50, "Name cannot be more than 50 characters"],
+    },
+    feedback: {
+      type: String,
+      required: [true, "Feedback text is required"],
+      trim: true,
+      minlength: [10, "Feedback must be at least 10 characters"],
+      maxlength: [1000, "Feedback cannot exceed 1000 characters"],
+    },
+  },
+  { timestamps: true },
+);
+
+module.exports = mongoose.model("Feedback", feedbackSchema);
