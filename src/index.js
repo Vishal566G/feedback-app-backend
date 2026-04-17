19c5caaf8bf6dbe03c7023f900938eef2fa39409
Vishal <visku199@gmail.com>
2026-04-18 00:21:00 +0530

    minor update

--- src/index.js
+++ src/index.js
@@ -12,8 +12,6 @@ app.use(
     origin: [
       "http://localhost:5173",
       "https://feedback-app-frontend-theta.vercel.app/",
-      "https://feedback-app-frontend-git-main-vishals-projects-b5051232.vercel.app/",
-      "https://feedback-app-frontend-3bylzbclw-vishals-projects-b5051232.vercel.app/",
     ],
   }),
 );
