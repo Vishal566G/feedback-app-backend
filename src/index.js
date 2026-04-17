3b5d99a6848808077df8d1d6201fd7e41e3bdaa1
Vishal <visku199@gmail.com>
2026-04-18 00:17:18 +0530

    Bypass cors in production

--- src/index.js
+++ src/index.js
@@ -7,7 +7,16 @@ const cors = require("cors");
 const feedbackRoutes = require("./routes/feedback");
 
 // Middleware
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://feedback-app-frontend-theta.vercel.app/",
+      "https://feedback-app-frontend-git-main-vishals-projects-b5051232.vercel.app/",
+      "https://feedback-app-frontend-3bylzbclw-vishals-projects-b5051232.vercel.app/",
+    ],
+  }),
+);
 app.use(express.json());
 
 // Routes
